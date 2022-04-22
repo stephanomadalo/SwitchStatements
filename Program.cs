50fb38187055acdca5c093f8b4e7b1c22a4510e7
Stephen Madalo <stephenmadalo89@gmail.com>
2022-04-21 22:19:09 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SwitchStatements 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What was your favorite school subject?");
+            var answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "Math":
+                    Console.WriteLine("Math is pretty cool");
+                    break;
+                case "Science":
+                    Console.WriteLine("I love science :)");
+                    break;
+                case "Social Studies":
+                    Console.WriteLine("Don't lie");
+                    break;
+                case "English":
+                    Console.WriteLine("meh");
+                    break;
+                case "Phys Ed":
+                    Console.WriteLine("Running laps sucked :'(");
+                    break;
+                default:
+                    Console.WriteLine("I ain't heard of that one super chief");
+                    break;
+            }
+
+
+        }
+    }
+}
